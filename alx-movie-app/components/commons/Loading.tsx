4435b94d3991c5a4dd68d0f76fbf84738d193fba
Obaro Awowede <akpobaroawowede@gmail.com>
2025-07-04 15:02:52 +0100

    done with final task

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -1,29 +1,12 @@
-import React from 'react';
-
 const Loading: React.FC = () => {
   return (
-    <div className="flex items-center justify-center p-4">
-      <svg
-        className="animate-spin h-8 w-8 text-gray-500"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        />
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-        />
-      </svg>
-      <span className="ml-2 text-gray-700">Loading...</span>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-6xl font-semibold text-white mb-4 animate-pulse">
+          Loading...
+        </h1>
+        <p className="text-lg text-gray-300">Please wait, we&apos;re getting next set of movies ready for you.</p>
+      </div>
     </div>
   );
 };
--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
 
-export interface MovieCardProps {
-  title: string;
-  posterUrl: string;
-  releaseYear?: number;
-  onClick?: () => void;
-}
-
-const MovieCard: React.FC<MovieCardProps> = ({ title, posterUrl, releaseYear, onClick }) => {
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
   return (
-    <div
-      className="max-w-xs bg-white rounded-lg shadow-md overflow-hidden cursor-pointer hover:shadow-lg transition-shadow"
-      onClick={onClick}
-    >
-      <img src={posterUrl} alt={`${title} Poster`} className="w-full h-64 object-cover" />
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">{title}</h3>
-        {releaseYear && <p className="text-sm text-gray-500">{releaseYear}</p>}
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
       </div>
     </div>
-  );
-};
+  )
+}
+
+export default MovieCard
